232ebacd2527d383b241e72234a032e13bb6753a
amy <amyniovi@gmail.com>
2016-06-01 18:40:50 +0100

    added multiple controllers in myChat.controllers module added delete functionality for single chat ( no state, as no DB can work on that later) working on popup controller and showing single chat data.

--- www/app/app.js
+++ www/app/app.js
@@ -1,111 +1,125 @@
-angular.module("myChatApp", ["ionic"])
+angular.module("myChatApp", ["ionic", "myChatApp.controllers"])
 
-.run(function($ionicPlatform){
+    .run(function ($ionicPlatform) {
 
-$ionicPlatform.ready(
-function(){
+        $ionicPlatform.ready(
+            function () {
 
-	if(window.cordova && window.cordova.plugins.Keyboard)
-	{
-		cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-	}
-	if(window.StatusBar){
-		StatusBar.styleDefault();
-	}
-});
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
 
-})
-
-
-.config(function($stateProvider,$urlRouterProvider){
-
-	$stateProvider
-
-    .state(
-		"home", {
-			url: "/home",
-			abstract : true,
-			templateUrl:"app/home/home.html"
-		})
-
-	.state(
-		"home.chats",	{
-			url:"/chats",
-			views:{
-				"tab-chats" : {
-					templateUrl: "app/home/chats.html"
-				}
-			}
-		})
-	.state(
-		"home.contacts",	{
-			url:"/contacts",
-			views:{
-				"tab-contacts" : {
-					templateUrl: "app/home/contacts.html"
-				}
-			}
-		})
-
-	.state("home.settings",	{
-			url:"/settings",
-			views:{
-				"tab-settings" : {
-					templateUrl: "app/home/settings.html"
-				}
-			}
-	})
-	.state("app", {
-      url: "/app",
-      abstract: true,
-      templateUrl: "app/layout/menu-layout.html"
     })
-    .state("app.paymentMethod", {
-      url: "/selectMethod",
-      views: {
-        "mainContent": {
-          templateUrl: "app/payment/selectMethod.html"
-        }
-      }
-    })
-    .state("app.paymentList",{
-      url:"/paymentList",
-      views:{"mainContent":{
-        templateUrl:"app/payment/paymentList.html"
-      }}
-    })
-      .state("app.payment",{
-        url:"/payment/:id",
-        views:{"mainContent":{
-          templateUrl:"app/payment/payment.html"
-        }}
-      }
-    )
-    .state("app.privacy", {
-      url: "/privacy",
-      views: {
-        "mainContent": {
-          templateUrl: "app/security/privacy.html"
-        }
-      }
-    })
-    .state("app.reviews", {
-      url: "/reviews",
-      views: {
-        "mainContent": {
-          templateUrl: "app/reviews/reviews.html"
-        }
-      }
+
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+            .state(
+                "home", {
+                    url: "/home",
+                    abstract: true,
+                    templateUrl: "app/home/home.html"
+                })
+
+            .state(
+                "home.chats", {
+                    url: "/chats",
+                    views: {
+                        "tab-chats": {
+                            templateUrl: "app/home/chats.html"
+                        }
+                    }
+                })
+            .state(
+                "home.chat", {
+                    url: "/chat/:id",
+                    views: {
+                        "chat-detail": {
+                            templateUrl: "app/home/chat-detail.html"
+                        }
+
+                    }
+                }
+            )
+            .state(
+                "home.contacts", {
+                    url: "/contacts",
+                    views: {
+                        "tab-contacts": {
+                            templateUrl: "app/home/contacts.html"
+                        }
+                    }
+                })
+
+            .state("home.settings", {
+                url: "/settings",
+                views: {
+                    "tab-settings": {
+                        templateUrl: "app/home/settings.html"
+                    }
+                }
+            })
+            .state("app", {
+                url: "/app",
+                abstract: true,
+                templateUrl: "app/layout/menu-layout.html"
+            })
+            .state("app.paymentMethod", {
+                url: "/selectMethod",
+                views: {
+                    "mainContent": {
+                        templateUrl: "app/payment/selectMethod.html"
+                    }
+                }
+            })
+            .state("app.paymentList", {
+                url: "/paymentList",
+                views: {
+                    "mainContent": {
+                        templateUrl: "app/payment/paymentList.html"
+                    }
+                }
+            })
+            .state("app.payment", {
+                    url: "/payment/:id",
+                    views: {
+                        "mainContent": {
+                            templateUrl: "app/payment/payment.html"
+                        }
+                    }
+                }
+            )
+            .state("app.privacy", {
+                url: "/privacy",
+                views: {
+                    "mainContent": {
+                        templateUrl: "app/security/privacy.html"
+                    }
+                }
+            })
+            .state("app.reviews", {
+                url: "/reviews",
+                views: {
+                    "mainContent": {
+                        templateUrl: "app/reviews/reviews.html"
+                    }
+                }
+            });
+
+
+        $urlRouterProvider.otherwise("/home/chats");
     });
 
-
-$urlRouterProvider.otherwise("/home/chats");
-});
-
 /*
-activating states:
-<a href="#/foo/bar">Go To</a>      //actual url
-<a ui-sref ="foo.bar">Go To</a>   //state name
-$state.go("foo.bar");  //javascript (name of state)
-*/
+ activating states:
+ <a href="#/foo/bar">Go To</a>      //actual url
+ <a ui-sref ="foo.bar">Go To</a>   //state name
+ $state.go("foo.bar");  //javascript (name of state)
+ */
--- www/app/services/chatService.js
+++ www/app/services/chatService.js
@@ -1,45 +1,62 @@
+(function () {
 
-(function(){
+    angular.module("myChatApp")
 
-angular.module("myChatApp")
+        .factory('chatService', function () {
 
-    .factory('chatService', function(){
+            var Chats = [
+                {
+                    id: 0,
+                    name: "Ben Obama",
+                    lastText: "where you at",
+                    pic: "img/ben.png"
+                },
+                {
+                    id: 1,
+                    name: "Mike Flynn",
+                    lastText: "cool ...catch you lr",
+                    pic: "img/mike.png"
+                },
+                {
+                    id: 2,
+                    name: "Adam Weirdo",
+                    lastText: "I m running late ...",
+                    pic: "img/adam.jpg"
+                }
+            ];
 
-        var Chats = [
-            {
-                id:0,
-                name:"Ben Obama",
-                lastText: "where you at",
-                pic:"img/ben.png"
-            },
-            {
-                id:1,
-                name:"Mike Flynn",
-                lastText: "cool ...catch you lr",
-                pic:"img/mike.png"
-            },
-            {
-                id:2,
-                name:"Adam Weirdo",
-                lastText: "I m running late ...",
-                pic:"img/adam.jpg"
-            }
-        ];
+            return {
 
-        return {
+                fetchAll: function () {
 
-            fetchAll : function (){
+                    return Chats;
+                },
 
-                return Chats;
-            },
+                get: function (id) {
 
-            get : function(){}
+                    for (var i = 0; i < Chats.length; i++) {
+                        if (Chats[i].id === id)
+                            return Chats[i];
+                    }
+                    return null;
+                },
 
-        };
+                delete: function (id) {
+
+                    var chat = Chats.find(function (obj) {
+                        return obj.id === id;
+                    });
+
+                    console.log("char to delete! : ", chat);
+
+                    if (chat != null && chat != undefined)
+                        Chats.splice(chat.index, 1);
+                }
+
+            };
 
 
-    });
-
+        });
 
 
 }());
